f795d3ecd0b9cfac06bdf371488ab76cf3fc5a5d
aburahan8721 <aburaihan8721@gmail.com>
2024-12-11 18:49:34 +0600

    some changes

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl:
+    process.env.NODE_ENV === 'production'
+      ? import.meta.env.VITE_BASE_URL
+      : 'http://localhost:5000',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+export const baseApi = createApi({
+  reducerPath: 'baseApi',
+  baseQuery: baseQuery,
+  tagTypes: [],
+  endpoints: () => ({}),
+});
